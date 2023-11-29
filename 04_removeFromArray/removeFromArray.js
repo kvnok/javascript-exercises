5cf8825d1ce27e441b3e6b9f0523588c4893013a
Kevin Kroon <kkroon@student.codam.nl>
2023-11-29 20:58:45 +0100

    done until 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+	let newArr = arr;
+	for (var arg of args) {
+		newArr = newArr.filter(item => item !== arg);
+	}
+	return newArr;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(n) {
+	return parseFloat(((n - 32) * 5/9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(n) {
+	return parseFloat((n * 9/5 + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit
 };
