46570dc5088ef236212a38c8e2acc72fc6bcb437
Kevin Kroon <kevinjankroon@gmail.com>
2024-03-31 23:55:14 +0200

    edgecase fib

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,7 +1,10 @@
 const fibonacci = function(n) {
-	if (n === 0) {
+	if (n < 0) {
+		return 'OOPS';
+	}
+	if (n == 0) {
 		return 0;
-	} else if (n === 1) {
+	} else if (n == 1) {
 		return 1;
 	} else {
 		return fibonacci(n - 1) + fibonacci(n - 2);
